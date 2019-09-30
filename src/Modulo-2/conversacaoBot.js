ef1e61336b39861edd22fa85e614114e8d3541d1
ReehnataCarvalho <reehnataoliveira592@gmail.com>
2019-09-30 17:49:58 -0300

    bot via Console com pergunta e resposta

---
+++ src/Modulo-2/conversacaoBot.js
@@ -0,0 +1,25 @@
+/*
+* Arquivo: conversacaoBot.js
+* Data: 30/09/2019
+* Descrição: Desenvolvimento de um Bot via COnsole.
+* Autor: Renata Carvalho
+*/
+
+const builder = require('botbuilder');
+
+const connector = new builder.ConsoleConnector().listen();
+const bot = new builder.UniversalBot(connector);
+
+bot.dialog('/', [
+    (session) => {
+        builder.Prompts.text(session, 'Oi! Tudo Bem ?');
+    },
+    (session) => {
+        builder.Prompts.text(session, 'Qual é o seu nome?')
+    },
+
+    (session, results) => {
+        let msg = results.response;
+        session.send(`Oi ${msg}! Em que posso ajudar?`)
+    }
+]);
